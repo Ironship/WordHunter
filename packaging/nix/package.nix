0596dbcb39f0309226c31d9a5272c6d820de1008
Ironship <25068602+Ironship@users.noreply.github.com>
2026-08-07 10:54:31 +0200

    chore: update package store metadata for 1.0.10
    - metainfo releases (linux + flatpak), debian changelog
    - chocolatey nuspec, scoop manifest, AUR PKGBUILD, nix package version+URL
    - flatpak cargo sources regenerated (no dependency changes)

--- packaging/nix/package.nix
+++ packaging/nix/package.nix
@@ -6,7 +6,7 @@
 
 let
   pname = "wordhunter";
-  version = "1.0.8";
+  version = "1.0.10";
 
   src = fetchurl {
     url = "https://github.com/Ironship/WordHunter/releases/download/WordHunter${version}/WordHunter-${version}-x86_64.AppImage";
